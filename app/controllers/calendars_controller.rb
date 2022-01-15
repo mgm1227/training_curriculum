69519b81bf5a502082349cf2a419ca4cf8a864b7
mgm1227 <chory.chory.c12@gmail.com>
2022-01-15 16:23:15 +0900

    Issue1-3 レビュー変更1

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,8 +2,8 @@ class CalendarsController < ApplicationController
 
   # １週間のカレンダーと予定が表示されるページ
   def index
-    getWeek
-    @week_plan = Plan.new
+    get_week
+    @plan = Plan.new
   end
 
   # 予定の保存
